fb85327aeecb82c5059f0b4153e6a4f6e654366f
Idir26 <idir.houmel@u-psud.fr>
2018-11-17 20:09:05 +0100

    TP1 added

--- TP1/Compteur.java
+++ TP1/Compteur.java
@@ -1,25 +1,41 @@
+import java.util.*;
+
 public class Compteur extends Thread {
 	public void run(){
 		for (int i=0; i<10; i++){
-		int hasard = (int)(Math.random()*5000);
+		System.out.println(" Thread : "+this.getName()+" : "+i);
+		Random rand = new Random();
+		int hasard = rand.nextInt(5000); ;
+		
 		try {
 			sleep(hasard);
 		}catch (Exception e) {
 			e.printStackTrace();
 	}
+	}
 		
-		System.out.println("Thread "+ this.getName() +"a fini de compter jusqu'à 10");
-		System.out.flush();
+		System.out.println("Thread "+ this.getName() +" a fini de compter jusqu'à 10");
+		
+	
 	
 	}
-	}
 	public static void main (String[] args){
+
 		int n = Integer.parseInt(args[0]);
 		
 		for (int i=0; i<n; i++){
-		Thread th = new Compteur();    
-		th.start();	
-		System.out.flush();
+			Thread th = new Compteur();    
+			th.start();
+		try {
+	
+			th.join();
+	
+		}
+		catch  (Exception e) {
+			e.printStackTrace();
+	
+		} 
+			
 		
 	}
 	}
